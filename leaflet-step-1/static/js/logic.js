3397febf687334c6ecc2df6ad12efb087ced2886
ddurnell <davereally@gmail.com>
2020-04-03 13:59:13 -0600

    Added inital legend

--- leaflet-step-1/static/js/logic.js
+++ leaflet-step-1/static/js/logic.js
@@ -23,20 +23,46 @@ d3.json(url, function(data) {
 // make the map
 function createMap(quakes) {
     // Adding tile layer to the map
-    var streets = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    var outdoors = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
         attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
         maxZoom: 18,
-        id: "mapbox.streets",
+        id: "mapbox.outdoors",
         accessToken: API_KEY
     });
 
-    // Creating map object
+    // Hold our base layers
+    var baseMaps = {
+        "Outdoors": outdoors
+      };
+    
+      // Create overlay object to hold our overlay layer
+      var overlayMaps = {
+        Earthquakes: quakes
+      };
+
+    // Create initial map
     var myMap = L.map("map", {
         center: [34.05, -118.24], // los angeles
         zoom: 4, // pretty wide
-        layers: [streets, quakes]
+        layers: [outdoors, quakes]
     });
-}
+    
+    // // Add the layer control to the map
+    // L.control.layers(baseMaps, overlayMaps, {
+    //     collapsed: false
+    //   }).addTo(myMap);
+  
+    // Add a legend
+    var info = L.control({
+        position: "bottomright"
+    });
+    info.onAdd = function(){
+        var div = L.DomUtil.create("div","legend");
+        return div;
+    }
+    info.addTo(myMap);
+    document.querySelector(".legend").innerHTML=displayLegend();
+};
 
 
 function chooseColor(mag){
@@ -70,6 +96,35 @@ function makeMarker(feature, location){
     return L.circleMarker(location, options);
 };
     
+function displayLegend(){
+    var legendInfo = [{
+        limit: "Mag: 0-1",
+        color: "green"
+    },{
+        limit: "Mag: 1-2",
+        color: "greenyellow"
+    },{
+        limit:"Mag: 2-3",
+        color:"gold"
+    },{
+        limit:"Mag: 3-4",
+        color:"DarkOrange"
+    },{
+        limit:"Mag: 4-5",
+        color:"Peru"
+    },{
+        limit:"Mag: 5+",
+        color:"red"
+    }];
 
+    var strng = "";
+   
+    for (i = 0; i < legendInfo.length; i++){
+        strng += "<p style = \"background-color: "+legendInfo[i].color+"\">"+legendInfo[i].limit+"</p> ";
+    }
+    
+    return strng;
+
+}
 
 
